968c16b43ec2cec801b628146138d6777568da27
EmanuelFonseca3839 <emanuelfonseca96@gmail.com>
2019-01-15 16:08:25 +0000

    w

--- bd_connect.php
+++ bd_connect.php
@@ -2,11 +2,13 @@
 $servername= "localhost";
 $user= "root";
 $pass= "";
+$database ="atw";
 
-$conn = new mysqli($servername,$user, $pass);
+$conn = mysqli_connect($servername,$user, $pass, $database);
 
 if ($conn -> connect_error){
     die ("Connection failed: " . $conn -> connect_error);
 }
 echo "Connected successfully";
+
 ?>
--- login.php
+++ login.php
@@ -1,3 +1,4 @@
 <?php
+include("bd_connect.php");
 
 ?>
