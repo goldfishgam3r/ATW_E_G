29793bceb37013382457ff28de18081551a05bf8
EmanuelFonseca3839 <emanuelfonseca96@gmail.com>
2019-01-15 18:51:33 +0000

    Update login.php

--- login.php
+++ login.php
@@ -2,4 +2,10 @@
 include("bd_connect.php");
 session_start();
 
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $myuser = mysqli_real_escape_string($conn, $_POST['user']);
+    $mypass = mysqli_real_escape_string($conn, $_POST['pass']);
+
+    $sql = "SELECT idu FROM utilizador WHERE username = '$myuser' and senha= '$mypass'";
+}
 ?>
