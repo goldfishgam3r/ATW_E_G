e355d0a964e4f105c541b385e8f6c44c922e69be
EmanuelFonseca3839 <emanuelfonseca96@gmail.com>
2019-02-02 17:37:14 +0000

    Update evento.php

--- evento.php
+++ evento.php
@@ -6,12 +6,21 @@ $queryevento= "SELECT * FROM evento WHERE ide=1";
 $resultevento= mysqli_query($conn, $queryevento);
 $rowevento= mysqli_fetch_array($resultevento);
 
-foreach($rowevento as $evento){
+/*foreach($rowevento as $evento){
     echo $evento."|";
-}
+}*/
+
+$utilizador = $rowEvento ['idu'];//criador do artigo
+	
+	$queryOrg = "SELECT * FROM utilizador WHERE idu = '$utilizador'";
+	$resultOrg = mysqli_query ($conn, $queryOrg);
+	$rowOrg = mysqli_fetch_array ($resultOrg);
 //print_r($rowevento["ide"]);
 ?>
 <html>
-<link rel="stylesheet" type="text/css" href="/DataTables/datatables.css">
-<script type="text/javascript" charset="utf8" src="/DataTables/datatables.js"></script>
-<script>
+<head>
+    <link rel="stylesheet" type="text/css" href="/DataTables/datatables.css">
+    <script type="text/javascript" charset="utf8" src="/DataTables/datatables.js"></script>
+</head>
+<body>
+<table id="evento">
