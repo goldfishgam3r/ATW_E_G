434b9d47e2d8306ec2c2e03261ede4bfeebe45b6
EmanuelFonseca3839 <emanuelfonseca96@gmail.com>
2019-02-02 17:16:31 +0000

    Update evento.php

--- evento.php
+++ evento.php
@@ -1,9 +1,16 @@
 <?php
-require ("config.php");
+header ('Content-type: text/html; charset=utf-8'); 
+require("config.php");
 
-$queryevento= "SELECT * FROM evento";
+$queryevento= "SELECT * FROM evento WHERE ide=1";
 $resultevento= mysqli_query($conn, $queryevento);
 $rowevento= mysqli_fetch_array($resultevento);
-print($resultevento);
-exit;
+
+foreach($rowevento as $evento){
+    echo $evento."|";
+}
+//print_r($rowevento["ide"]);
 ?>
+<html>
+<link rel="stylesheet" type="text/css" href="/DataTables/datatables.css">
+ <script type="text/javascript" charset="utf8" src="/DataTables/datatables.js"></script>
