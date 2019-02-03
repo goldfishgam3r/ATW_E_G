840cb2e68c7fd2146e89b8ff9bd3f983b7d7e201
goldfishgam3r <golsfishgam3r@gmail.com>
2019-02-03 17:51:47 +0000

    Update menu.php

--- menu.php
+++ menu.php
@@ -8,11 +8,11 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">Help Running</a>
+        <a class="navbar-brand">Help Running</a>
       </div>
       <div id="navbar" class="navbar-collapse collapse">
         <ul class="nav navbar-nav">
-          <li class="active"><a href="index.html">Home</a></li>
+          <li class="active"><a href="index.php">Home</a></li>
           <li><a href="sobre.html">Sobre</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
